47c66819df19c57236c01ff9b70b4714561991c3
Megh Patel <https://github.com/Megharth/udaan19-website.git>
2023-04-01 19:35:03 +0530

    first

---
+++ backend/config/db.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    console.log("Here");
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useCreateIndex: true,
+      useFindAndModify: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
+  } catch (error) {
+    console.error(`Error: ${error.message}`.red.underline.bold);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
